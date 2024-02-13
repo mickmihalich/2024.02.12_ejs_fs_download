0db7f4fb1aa31cffdc7a194e1e55db11c62b5c9c
mickmihalich <mickmihalich@gmail.com>
2024-02-13 19:42:39 +0200

    before stream

--- routes/download.js
+++ routes/download.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 import { readFile } from 'fs/promises';
 
-const file = await readFile('\README.md', 'utf8'); //??? 
+//const file = await readFile('\README.md', 'utf8'); //??? 
 
 /* GET home page. */
 router.get('/', (req, res, next) => {    
     res.setHeader('Content-Type', 'application/octet-stream');
     res.setHeader('Content-Disposition', `attachment; filename="README.md"`);
-    res.send(file);
+    res.send(readFile('\README.md', 'utf8'));
 });
 
 export default router;
