53a45485541286c3c7c7c47d2c5d43c248bb4551
mickmihalich <mickmihalich@gmail.com>
2024-02-14 19:27:14 +0200

    rem Download Page

--- routes/download.js
+++ routes/download.js
@@ -5,7 +5,7 @@ import { readFile } from 'fs/promises';
 
 //const file = await readFile('\README.md', 'utf8'); //??? 
 
-/* GET home page. */
+/* GET Download Page. */
 router.get('/', (req, res, next) => {    
     res.setHeader('Content-Type', 'application/octet-stream');
     res.setHeader('Content-Disposition', `attachment; filename="README.md"`);
